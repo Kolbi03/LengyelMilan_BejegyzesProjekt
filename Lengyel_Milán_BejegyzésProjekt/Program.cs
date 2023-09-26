43efdc5464c464d02b341ea5b9e8188e191d9141
Lengyel Milán <lengyel.milan.hu@gmail.com>
2023-09-26 08:05:16 +0200

    Add project files.

---
+++ Lengyel_Milán_BejegyzésProjekt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lengyel_Milán_BejegyzésProjekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
